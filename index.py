335d0c850cd6b2e1cf6d7e330893917d6bbcf4b7
Farah <fairy2012007@gmail.com>
2025-09-08 16:52:49 +0000

    numpy basic work

---
+++ index.py
@@ -0,0 +1,41 @@
+# 1D array
+a = np.array([1, 2, 3])
+
+# 2D array
+b = np.array([[1, 2], [3, 4]])
+
+# Array of zeros
+zeros = np.zeros((2, 3))
+
+# Array of ones
+ones = np.ones((3, 3))
+
+# Identity matrix
+identity = np.eye(3)
+
+# Range of values
+arr = np.arange(0, 10, 2)
+
+# Random values
+random = np.random.rand(2, 2)
+
+
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Element-wise addition
+c = a + b
+
+# Element-wise multiplication
+d = a * b
+
+# Dot product
+dot = np.dot(a, b)
+
+# Square root
+sqrt = np.sqrt(a)
+
+# Mean and sum
+mean = np.mean(a)
+total = np.sum(a)
